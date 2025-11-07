28bc1f89eacf01e2828c4aa561b4053c3a5cc7c2
João Antonio David <95722136+joaoadavid@users.noreply.github.com>
2025-11-07 17:19:20 -0300

    Delete authors directory

--- authors/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .login_form import LoginForm
-from .register_form import RegisterForm
-__all__ = ["LoginForm", "RegisterForm"]
--- authors/views22/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-from .all import *
-from .dashboard_recipe import *
